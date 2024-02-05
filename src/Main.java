0dd8e09d0f7a019859de2fd8f2c69bf3ba5c5f62
oscar <oscar.cruanas@gracia.lasalle.cat>
2024-02-05 12:33:15 +0100

    Cambio 1: copiar menú del profe

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,65 @@
-
+import java.util.Scanner;
 
 public class Main {
+
+    Scanner input = new Scanner(System.in);
+
+
+
     public static void main(String[] args) {
 
+        Main main = new Main();
+        main.init();
     }
+
+
+
+    public void init(){
+        int menuItem = 0;
+        do {
+            System.out.println("TALLER DE REPARACIÓ DE VEHICLES");
+            System.out.println("[1] Donar d’alta nou client");
+            System.out.println("[2] Donar d’alta nou mecànic");
+            System.out.println("[3] Introduir nou vehicle");
+            System.out.println("[4] Crear fitxa de nova reparació");
+            System.out.println("[5] Sortir");
+            System.out.println("Sel·lecciona una opció: ");
+
+            if (input.hasNextInt()){
+                menuItem = input.nextInt();
+                switch (menuItem){
+                    case 1:
+                        System.out.println("Has triat donar d’alta nou client....");
+                        //insert code here
+                        break;
+                    case 2:
+                        System.out.println("Has triat donar d’alta nou mecànic....");
+                        //insert code here
+                        break;
+                    case 3:
+                        System.out.println("Has triat introduir nou vehicle....");
+                        //insert code here
+                        break;
+                    case 4:
+                        System.out.println("Has triat crear fitxa de nova reparació....");
+                        //insert code here
+                        break;
+                    case 5:
+                        System.out.println("Sortint....");
+                        break;
+                    default:
+                        System.out.println("Opció no vàlida");
+                }
+            }else{
+                System.out.println("Opció no vàlida");
+            }
+            input.nextLine();
+            System.out.println("");
+
+        }while(menuItem!=5);
+
+    }
+
+    //insert code here
+
 }
